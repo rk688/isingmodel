d78d5fffcf36249948814eeca817cc6ec2bc6003
Ruben Kuespert <rhtkue@pc47.tpi.uni-jena.de>
2019-06-05 13:59:24 +0200

    Merge Autokorrelation fuer WOLFF
    starten Binderkommulante

---
+++ binder.cpp
@@ -0,0 +1,13 @@
+#include <string>     // needed for strings
+#include <vector>     // needed for vectors
+#include <fstream>    // needed for "ifstream" and "ofstream" (file input/output)
+#include <sstream>    // needed for "stringstream"
+#include <iostream>   // needed for "cout" (console output)
+#include <iomanip>    // needed for "scientific" and "setw" format manipulators
+#include <cstdlib>    // needed for "rand" (random numbers)
+#include <algorithm>  // needed for "pow" (x^y)
+#include <cmath>
+#include <limits>
+#include <ctime>
+
+using namespace std;  // otherwise we would always have to write "std::vector" instead of just "vector"
